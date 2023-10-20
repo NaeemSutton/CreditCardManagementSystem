cd6d06502cac0aa00a15b40b288920b4a4ebe9c1
Naeem Sutton <107595797+NaeemSutton@users.noreply.github.com>
2023-10-20 12:44:27 -0700

    Update Charge.h

--- Charge.h
+++ Charge.h
@@ -1,18 +1,3 @@
-//Copyright [2023]
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * File:   Charge.h
- * Author: hloi
- *
- * Created on November 24, 2018, 12:38 AM
- */
-
 #ifndef CHARGE_H
 #define CHARGE_H
 #include <string>
